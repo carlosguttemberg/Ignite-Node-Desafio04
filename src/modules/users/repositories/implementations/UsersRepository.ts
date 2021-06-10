6fa1ce689ac26a121e396021821ecfa0a6a4ee73
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-06-09 21:40:27 -0300

    Finalizando o exercicio 04

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,69 @@
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findById(id: string): User | undefined {
+    const user = this.users.find((userList) => userList.id === id);
+
+    return user;
+  }
+
+  findByEmail(email: string): User | undefined {
+    const user = this.users.find((userList) => userList.email === email);
+
+    return user;
+  }
+
+  turnAdmin(receivedUser: User): User {
+    const userUpdated = this.users.map((user) => {
+      if (user.id === receivedUser.id) {
+        Object.assign(user, {
+          admin: true,
+          updated_at: new Date(),
+        });
+      }
+      return user;
+    });
+
+    this.users = userUpdated;
+
+    return this.findById(receivedUser.id);
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+
+export { UsersRepository };
